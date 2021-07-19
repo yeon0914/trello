492f8dfb1771de43681d0e3dccc19d19c5a35141
chyeuni <chyeuni99@naver.com>
2021-07-19 09:10:15 +0900

    axios 추가, GET/health 요청 로직 재구현

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -4,7 +4,13 @@
     <div>
       Board List:
       <div v-if="loading">Loading...</div>
-      <div v-else>Api result: {{ apiRes }}</div>
+      <div v-else>
+        Api result:
+        <pre>{{ apiRes }}</pre>
+      </div>
+      <div v-if="error">
+        <pre>{{ error }}</pre>
+      </div>
       <ul>
         <li><router-link to="/b/1">Board 1</router-link></li>
         <li><router-link to="/b/2">Board 2</router-link></li>
@@ -14,11 +20,13 @@
 </template>
 
 <script>
+import axios from "axios";
 export default {
   data() {
     return {
       loading: false,
-      apiRes: ""
+      apiRes: "",
+      error: ""
     };
   },
   created() {
@@ -27,17 +35,18 @@ export default {
   methods: {
     fetchData() {
       this.loading = true;
-      const req = new XMLHttpRequest();
-      req.open("GET", "http://localhost:3000/health");
-      req.send();
-      req.addEventListener("load", () => {
-        this.loading = false;
-        this.apiRes = {
-          status: req.status,
-          statusText: req.statusText,
-          response: JSON.parse(req.response)
-        };
-      });
+
+      axios
+        .get("http://localhost:3000/health")
+        .then(res => {
+          this.apiRes = res.data;
+        })
+        .catch(res => {
+          this.error = res.response.data;
+        })
+        .finally(() => {
+          this.loading = false;
+        });
     }
   }
 };
