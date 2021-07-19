a62d2db792655113146012a4e27d2d5f2b85dc17
chyeuni <chyeuni99@naver.com>
2021-07-19 09:15:51 +0900

    GET /boards 연동

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -6,11 +6,11 @@
       <div v-if="loading">Loading...</div>
       <div v-else>
         Api result:
-        <pre>{{ apiRes }}</pre>
+        <div v-for="b in boards" :key="b.id">
+          {{ b }}
+        </div>
       </div>
-      <div v-if="error">
-        <pre>{{ error }}</pre>
-      </div>
+
       <ul>
         <li><router-link to="/b/1">Board 1</router-link></li>
         <li><router-link to="/b/2">Board 2</router-link></li>
@@ -25,8 +25,7 @@ export default {
   data() {
     return {
       loading: false,
-      apiRes: "",
-      error: ""
+      boards: []
     };
   },
   created() {
@@ -37,12 +36,12 @@ export default {
       this.loading = true;
 
       axios
-        .get("http://localhost:3000/health")
+        .get("http://localhost:3000/boards")
         .then(res => {
-          this.apiRes = res.data;
+          this.boards = res.data;
         })
         .catch(res => {
-          this.error = res.response.data;
+          this.$router.replace("/login");
         })
         .finally(() => {
           this.loading = false;
