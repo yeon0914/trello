0b430f51165028bc6300ba895589ce76ab312220
chyeuni <chyeuni99@naver.com>
2021-07-20 15:58:13 +0900

    Login onSubmit() 구현

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,70 @@
 <template>
   <div id="app">
-    <Navbar/> 
+    <Navbar />
     <router-view></router-view>
   </div>
 </template>
 
 <script>
-import Navbar from './components/Navbar.vue'
+import Navbar from "./components/Navbar.vue";
 export default {
   components: { Navbar },
-  name: 'app',
-  compoents: {Navbar},
-  data () {
-    return {
-       
-    }
+  name: "app",
+  compoents: { Navbar },
+  data() {
+    return {};
   }
-}
+};
 </script>
 
 <style>
+html,
+body,
+#app {
+  height: 100%;
+  margin: 0px;
+}
+#app {
+  display: flex;
+  flex-direction: column;
+}
+.container {
+  flex-grow: 1;
+  position: relative;
+}
+.btn {
+  border-radius: 3px;
+  padding: 6px 8px;
+  background-color: #e2e4e6;
+  border: none;
+  display: inline-block;
+  color: #fff;
+  font-size: 14px;
+  line-height: 20px;
+  font-weight: 700;
+  cursor: pointer;
+}
+.btn-success {
+  background-color: #5aac44;
+  box-shadow: 0 1px 0 #519839;
+}
+.font-control {
+  width: 100%;
+  box-sizing: border-box;
+  background-color: #e2e4e6;
+  border: 1px solid #cdd2d4;
+  border-radius: 3px;
+  display: block;
+  margin-bottom: 12px;
+  padding: 6px 8px;
+  transition: background-color 0.3s;
+}
+input[type="text"].form-control,
+input[type="password"].form-control,
+textarea.form-control {
+  font-size: 14px;
+}
+.form-control:focus {
+  background-color: #fff;
+}
+</style>
