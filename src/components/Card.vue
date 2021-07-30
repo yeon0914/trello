896542a904798b1bb39758bd32634a3d9503f2af
chyeuni <chyeuni99@naver.com>
2021-07-30 12:55:23 +0900

    card/show-routing

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,15 +1,19 @@
 <template>
-  <div>
+  <Modal>
     Card
     <div v-if="loading">loading card...</div>
     <div v-else>
       <div>cid: {{ cid }}</div>
     </div>
-  </div>
+  </Modal>
 </template>
 
 <script>
+import Modal from "./Modal.vue";
 export default {
+  components: {
+    Modal
+  },
   data() {
     return {
       cid: 0,
