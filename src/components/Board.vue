2040eef9f0130a41ccc595c05508490a31fc463b
chyeuni <chyeuni99@naver.com>
2021-08-06 10:00:25 +0900

    board/settings

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -4,6 +4,9 @@
       <div class="board">
         <div class="board-header">
           <span class="board-title">{{board.title}}</span>
+          <a class="board-header-btn show-menu" href="" @click.prevent="onShowSettings">
+            ... Show Menu
+          </a>
         </div>
         <div class="list-section-wrapper">
           <div class="list-section">
@@ -14,16 +17,19 @@
         </div>
       </div>
     </div>
-     <router-view></router-view>
+      <BoardSettings v-if="isShowBoardSettings" />
+      <router-view></router-view>
   </div>
 </template>
 
 <script>
 import { mapState, mapActions, mapMutations } from "vuex";
 import List from "./List.vue";
+import BoardSettings from "./BoardSettings.vue";
 import dragger from "../utils/dragger";
+
 export default {
-  components: { List },
+  components: { List, BoardSettings },
   data() {
     return {
       bid: 0,
@@ -33,19 +39,21 @@ export default {
   },
   computed: {
     ...mapState({
-      board: "board"
+      board: "board",
+      isShowBoardSettings: "isShowBoardSettings"
     })
   },
   created() {
     this.fetchData().then(() => {
       this.SET_THEME(this.board.bgColor);
     });
+    this.SET_IS_SHOW_BOARD_SETTINGS(false);
   },
   updated() {
     this.setCardDragabble();
   },
   methods: {
-    ...mapMutations(["SET_THEME"]),
+    ...mapMutations(["SET_THEME", "SET_IS_SHOW_BOARD_SETTINGS"]),
     ...mapActions(["FETCH_BOARD", "UPDATE_CARD"]),
     fetchData() {
       this.loading = true;
@@ -77,6 +85,9 @@ export default {
         else if (next && prev) targetCard.pos = (prev.pos + next.pos) / 2;
         this.UPDATE_CARD(targetCard);
       });
+    },
+    onShowSettings() {
+      this.SET_IS_SHOW_BOARD_SETTINGS(true);
     }
   }
 };
